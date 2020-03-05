b1d2e779426cc6407bd6dd158388608a04f72d47
aldenkane <akane2@nd.edu>
2020-03-05 01:04:41 -0500

    Following Their Instructions

--- sourceCode/lidar_Playground.py
+++ sourceCode/lidar_Playground.py
@@ -3,17 +3,24 @@ import smbus
 # Declare Globals and Device Parameters
 CHANNEL = 1
 DEVICE_ADDRESS = 0x62
+
 ACQ_COMMANDS = 0x00
+STATUS = 0x01
 FULL_DELAY_LOW = 0x10
 FULL_DELAY_HIGH = 0X11
+
 RESET_BYTE = 0x00
-BIASED_DISTANCE = 0X00
+BIASED_DISTANCE = 0X04
 
-# Declare Bus w/ SMbus
-bus = smbus.SMBus(CHANNEL)
 
-# Write Distance Command w/ Receiver Correction
+# Declare Bus w/ SMbus, Intialize I2C
+bus = smbus.SMBus(CHANNEL, STATUS)
+
+# Initialize LiDAR by Writing to Correction Distance Mode to ACQ Register
 bus.write_byte_data(DEVICE_ADDRESS, ACQ_COMMANDS, BIASED_DISTANCE)
+
+# Read Distance Data
+r_Stat = bus.read_byte_data(DEVICE_ADDRESS, STATUS)
 
 #Reset Measurements
 bus.write_byte_data(DEVICE_ADDRESS, FULL_DELAY_LOW, RESET_BYTE)
@@ -23,5 +30,6 @@ bus.write_byte_data(DEVICE_ADDRESS, FULL_DELAY_HIGH, RESET_BYTE)
 low_Distance = bus.read_byte_data(DEVICE_ADDRESS, FULL_DELAY_LOW)
 high_Distance = bus.read_byte_data(DEVICE_ADDRESS, FULL_DELAY_HIGH)
 
+print("Read Status Distance: " + str(r_Stat) + 'cm')
 print("Low Distance Byte: " + str(low_Distance) + " cm")
 print("Low Distance Byte: " + str(high_Distance) + " cm")
