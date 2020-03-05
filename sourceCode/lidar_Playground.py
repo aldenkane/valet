dff07b7e1bb9cb5a296281bf44290bce95ccef5a
aldenkane <akane2@nd.edu>
2020-03-05 00:49:38 -0500

    Working on I2C

--- sourceCode/lidar_Playground.py
+++ sourceCode/lidar_Playground.py
@@ -3,20 +3,25 @@ import smbus
 # Declare Globals and Device Parameters
 CHANNEL = 1
 DEVICE_ADDRESS = 0x62
-DISTANCE_LOW_BYTE = 0x10
-DISTANCE_HIGH_BYTE = 0X11
+ACQ_COMMANDS = 0x00
+FULL_DELAY_LOW = 0x10
+FULL_DELAY_HIGH = 0X11
 RESET_BYTE = 0x00
+BIASED_DISTANCE = 0X00
 
 # Declare Bus w/ SMbus
 bus = smbus.SMBus(CHANNEL)
 
+# Write Distance Command w/ Receiver Correction
+bus.write_byte_data(DEVICE_ADDRESS, ACQ_COMMANDS, BIASED_DISTANCE)
+
 #Reset Measurements
-bus.write_byte_data(DEVICE_ADDRESS, DISTANCE_LOW_BYTE, RESET_BYTE)
-bus.write_byte_data(DEVICE_ADDRESS, DISTANCE_HIGH_BYTE, RESET_BYTE)
+bus.write_byte_data(DEVICE_ADDRESS, FULL_DELAY_LOW, RESET_BYTE)
+bus.write_byte_data(DEVICE_ADDRESS, FULL_DELAY_HIGH, RESET_BYTE)
 
 #Read device data
-low_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_LOW_BYTE)
-high_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_HIGH_BYTE)
+low_Distance = bus.read_byte_data(DEVICE_ADDRESS, FULL_DELAY_LOW)
+high_Distance = bus.read_byte_data(DEVICE_ADDRESS, FULL_DELAY_HIGH)
 
 print("Low Distance Byte: " + str(low_Distance) + " cm")
 print("Low Distance Byte: " + str(high_Distance) + " cm")
