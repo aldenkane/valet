850a726d5ca08f11edd6dc64c53e22391697250b
aldenkane <akane2@nd.edu>
2020-03-04 23:00:28 -0500

    Hopefully this lidar works

--- sourceCode/lidar_Playground.py
+++ sourceCode/lidar_Playground.py
@@ -1,14 +1,18 @@
 import smbus
 
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+# Declare Globals and Device Parameters
+CHANNEL = 1
+DEVICE_ADDRESS = 0x62
+DISTANCE_LOW_BYTE = 0x10
+DISTANCE_HIGH_BYTE = 0X11
 
-DEVICE_ADDRESS = 0x62      #7 bit address (will be left shifted to add the read write bit)
-DEVICE_REG_MODE1 = 0x00
-distance_Low_Byte = 0x10
-DEVICE_REG_LEDOUT0 = 0x1d
+# Declare Bus w/ SMbus
+bus = smbus.SMBus(CHANNEL)
 
 #Read device data
 
-read = bus.read_byte_data(DEVICE_ADDRESS, distance_Low_Byte)
+low_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_LOW_BYTE)
+high_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_HIGH_BYTE)
 
-print(read)
+print("Low Distance Byte: " + str(low_Distance) + " cm")
+print("Low Distance Byte: " + str(high_Distance) + " cm")
