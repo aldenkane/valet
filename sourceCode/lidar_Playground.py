35b9269d1764ca61ae2a9bb8852bc468ca0e97f4
aldenkane <akane2@nd.edu>
2020-03-04 23:02:23 -0500

    Resetting Bytes Now

--- sourceCode/lidar_Playground.py
+++ sourceCode/lidar_Playground.py
@@ -5,12 +5,16 @@ CHANNEL = 1
 DEVICE_ADDRESS = 0x62
 DISTANCE_LOW_BYTE = 0x10
 DISTANCE_HIGH_BYTE = 0X11
+RESET_BYTE = 0x00
 
 # Declare Bus w/ SMbus
 bus = smbus.SMBus(CHANNEL)
 
-#Read device data
+#Reset Measurements
+bus.write_byte_data(DEVICE_ADDRESS, RESET_BYTE)
+bus.write_byte_data(DEVICE_ADDRESS, RESET_BYTE)
 
+#Read device data
 low_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_LOW_BYTE)
 high_Distance = bus.read_byte_data(DEVICE_ADDRESS, DISTANCE_HIGH_BYTE)
 
